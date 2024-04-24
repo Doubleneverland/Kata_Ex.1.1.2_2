d16b900f975f9e835aa06b919db384b61cc70b50
Nikolay <flpaladyshev@gmail.com>
2024-04-24 11:46:36 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.dropUsersTable();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Tantyana", "Fedorova", (byte) 25);
+        userDaoJDBC.saveUser("Tantyana", "Fedorova", (byte) 25);
+        userDaoJDBC.saveUser("Olga", "Petrova",(byte) 34);
+        userDaoJDBC.saveUser("Igor", "Serov", (byte) 26);
+        userDaoJDBC.saveUser("Nikolay", "Egorov", (byte) 23);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.removeUserById(2);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.cleanUsersTable();
+        // реализуйте алгоритм здесь
+    }
+}
